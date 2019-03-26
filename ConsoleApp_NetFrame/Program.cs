7c7daf99780ce5549f9080764134ce4dd2db272d
Shirlei <luolivver@hotmail.com>
2019-03-26 10:09:33 -0300

    Adding ConsoleApp_NetFrame project at solution

---
+++ ConsoleApp_NetFrame/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_NetFrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            //Navigate to google page
+            driver.Navigate().GoToUrl("http:www.google.com");
+
+            //Maximize the window
+            driver.Manage().Window.Maximize();
+
+            //Find the Search text box using xpath
+            IWebElement element = driver.FindElement(By.XPath("//*[@title='Search']"));
+
+            //Enter some text in search text box
+            element.SendKeys("learn-automation");
+
+            //Close the browser
+            driver.Close();
+        }
+    }
+}
